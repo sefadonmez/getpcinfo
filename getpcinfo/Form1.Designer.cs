3b79782f5e8518019ecfe838376fa409fe795ac0
sefadonmez <sefadonmez@hotmail.com>
2024-04-21 13:12:09 +0300

    Finish

--- getpcinfo/Form1.Designer.cs
+++ getpcinfo/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxInfoList = new System.Windows.Forms.ListBox();
+            this.buttonTXTexport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxInfoList
+            // 
+            this.listBoxInfoList.FormattingEnabled = true;
+            this.listBoxInfoList.Location = new System.Drawing.Point(12, 12);
+            this.listBoxInfoList.Name = "listBoxInfoList";
+            this.listBoxInfoList.Size = new System.Drawing.Size(359, 420);
+            this.listBoxInfoList.TabIndex = 0;
+            // 
+            // buttonTXTexport
+            // 
+            this.buttonTXTexport.Location = new System.Drawing.Point(283, 438);
+            this.buttonTXTexport.Name = "buttonTXTexport";
+            this.buttonTXTexport.Size = new System.Drawing.Size(88, 23);
+            this.buttonTXTexport.TabIndex = 1;
+            this.buttonTXTexport.Text = "Save To .txt";
+            this.buttonTXTexport.UseVisualStyleBackColor = true;
+            this.buttonTXTexport.Click += new System.EventHandler(this.buttonTXTexport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(377, 464);
+            this.Controls.Add(this.buttonTXTexport);
+            this.Controls.Add(this.listBoxInfoList);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "All Pc Info";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxInfoList;
+        private System.Windows.Forms.Button buttonTXTexport;
     }
 }
 
--- getpcinfo/Form1.cs
+++ getpcinfo/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,90 @@ namespace getpcinfo
         public Form1()
         {
             InitializeComponent();
+            GetSystemInfo();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void GetSystemInfo()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    listBoxInfoList.Items.Add("Computer Name: " + queryObj["Name"]);
+                    listBoxInfoList.Items.Add("Manufacturer: " + queryObj["Manufacturer"]);
+                    listBoxInfoList.Items.Add("Model: " + queryObj["Model"]);
+                    listBoxInfoList.Items.Add("System Type: " + queryObj["SystemType"]);
+                    listBoxInfoList.Items.Add("Total Physical Memory: " + queryObj["TotalPhysicalMemory"]);
+                }
+
+                searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    listBoxInfoList.Items.Add("Processor: " + queryObj["Name"]);
+                }
+
+                searcher = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    listBoxInfoList.Items.Add("OS Name: " + queryObj["Caption"]);
+                    listBoxInfoList.Items.Add("Version: " + queryObj["Version"]);
+                    listBoxInfoList.Items.Add("Serial Number: " + queryObj["SerialNumber"]);
+                }
+
+                searcher = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = 'TRUE'");
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    listBoxInfoList.Items.Add("MAC Address: " + queryObj["MACAddress"]);
+                    string[] addresses = (string[])queryObj["IPAddress"];
+                    if (addresses != null)
+                    {
+                        foreach (string address in addresses)
+                        {
+                            listBoxInfoList.Items.Add("IP Address: " + address);
+                        }
+                    }
+                }
+            }
+            catch (ManagementException e)
+            {
+                MessageBox.Show("An error occurred while querying for WMI data: " + e.Message);
+            }
+        }
+
+        private void buttonTXTexport_Click(object sender, EventArgs e)
+        {
+            SaveToText();
+        }
+
+
+         string getTime = DateTime.Now.ToLongDateString().ToString();
+        private void SaveToText()
+        {
+           
+            string textFileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, getTime + ".txt");
+            using (StreamWriter writer = new StreamWriter(textFileName))
+            {
+                writer.WriteLine("All PC Info List \t");
+                for (int i = 0; i < listBoxInfoList.Items.Count; i++)
+                {
+                    string ticari = listBoxInfoList.Items[i].ToString();
+                    writer.WriteLine($"{ticari}\t");
+                }
+            }
+
+            MessageBox.Show("Veriler metin dosyasına aktarıldı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            // Dosya yolunu aç
+            string directoryPath = Path.GetDirectoryName(textFileName);
+            System.Diagnostics.Process.Start(directoryPath);
+        }
+
     }
+
 }
